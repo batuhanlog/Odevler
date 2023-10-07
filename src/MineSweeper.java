a4d5f6a11db202d72bef06757ba8e94d201b475f
batuhanlog <mrkayahan66@gmail.com>
2023-10-07 19:56:37 +0300

    Update MineSweeper.java
    Düzenleme

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -1,107 +1,154 @@
 import java.util.Random;
 import java.util.Scanner;
 
-// Mayın tarlası oyununu temsil eden MineSweeper sınıfı
 public class MineSweeper {
-    int rowNumber, colNumber,size;
-    int[][] map;
-    int[][] board;
-    boolean game = true;
+    Scanner tara = new Scanner(System.in);
+    private int adetSatir;
+    private int adetSutun;
+    private String[][] mineMap;
+    private String[][] board;
+    private int sayac = 0;
+    private int satirNo;
+    private int sutunNo;
+    private boolean win;
+    private boolean durum;
 
-    Random rand=new Random();
-    Scanner scan=new Scanner(System.in);
-
-    // Mayın Tarlası sınıfının kurucu methodu
-    MineSweeper(int rowNumber,int colNumber){
-        this.rowNumber=rowNumber;
-        this.colNumber=colNumber;
-        this.map = new int [rowNumber][colNumber];
-        this.board=new int [rowNumber][colNumber];
-        this.size=rowNumber*colNumber;
-
+    // MineSweeper sınıfının kurucu metodu
+    MineSweeper(int adetSatir, int adetSutun) {
+        this.adetSatir = adetSatir;
+        this.adetSutun = adetSutun;
+        this.mineMap = new String[adetSatir][adetSutun];
+        this.board = new String[adetSatir][adetSutun];
+        fill(this.board); // Oyun tahtasını "-" ile doldur
+        fill(this.mineMap); // Mayın haritasını "-" ile doldur
     }
 
-    // Oyunun ana işleyişini yürüten method
+    // Rastgele bir satır ve sütun numarası üretme metodu
+    public void randomSayi() {
+        Random rdn = new Random();
+        this.satirNo = rdn.nextInt(this.adetSatir);
+
+        Random rdn2 = new Random();
+        this.sutunNo = rdn2.nextInt(this.adetSutun);
+    }
+
+    // Verilen bir diziyi "-" ile dolduran metot
+    public String[][] fill(String[][] dizi) {
+        for (int i = 0; i < this.adetSatir; i++) {
+            for (int j = 0; j < this.adetSutun; j++) {
+                dizi[i][j] = "-";
+            }
+        }
+        return dizi;
+    }
+
+    // Belirtilen koordinatta bir mayın bulunup bulunmadığını kontrol eden metot
+    public boolean isFind(String[][] arr, String value) {
+        if (arr[this.satirNo][sutunNo] == "*") {
+            return false;
+        }
+        return true;
+    }
+
+    // Oyunun ana mantığı olan metot
     public void run() {
-        int row,col,success=0;
-        prepareGame();
-        print(map);
-        System.out.println("Oyun başladı!");
-        while(game) {
-            print(board);
-            System.out.print("Satır Sayısı Giriniz : ");
-            row = scan.nextInt();
-            System.out.print("Sütun Sayısı Giriniz : ");
-            col = scan.nextInt();
-            if(row < 0 || row >= rowNumber) {
-                System.out.println("Yanlış Kordinat!");
-                continue;
-            }
-            if(col < 0 || col >= colNumber) {
-                System.out.println("Yanlış Kordinat!");
-                continue;
-            }
-
-            if(map[row][col]!= -1) {
-                check(row,col);
-                success++;
-                if(success == (size -(size/4))){
-                    System.out.println("TEBRİKLER HİÇ BİR MAYINA BASMADIN");
-                    break;
+        for (int j = 0; j < this.adetSatir; j++) {
+            for (int k = 0; k < this.adetSutun; k++) {
+                if (sayac < (this.adetSutun * this.adetSatir / 4)) {
+                    randomSayi();
+                    sayac++;
+                    if (isFind(this.board, "*")) {
+                        this.mineMap[satirNo][sutunNo] = "*";
+                        this.board[satirNo][sutunNo] = "*";
+                    } else sayac--;
                 }
-            }else {
-                game = false;
-                System.out.println("YANDIN! (Üzülme Artık Gazisin!)");
             }
+        }
+        printMineMap(); // Mayın haritasını ekrana bas
+        fill(this.board); // Oyun tahtasını sıfırla
+        printBoard(); // Oyun tahtasını ekrana bas
+        koordinatGir(); // Oyuncudan koordinat girmesini iste
+    }
+
+    private int durumSayac = 0;
+
+    // Oyuncunun koordinat girmesini ve oyunun ilerleyişini kontrol eden metot
+    public void koordinatGir() {
+        this.durum = true;
+        while (this.durum) {
+            int sayac = 0;
+            this.durumSayac++;
+            boolean kontrol = true;
+            int satir = 0;
+            int sutun = 0;
+            while (kontrol) {
+                System.out.println("Satır Giriniz : ");
+                satir = tara.nextInt();
+                System.out.println("Sütun Giriniz : ");
+                sutun = tara.nextInt();
+                if ((satir < 0 || satir > (this.adetSatir - 1)) || (sutun < 0 || sutun > (this.adetSutun - 1))) {
+                    System.out.println("Hatalı sayı girdiniz lütfen tekrar giriniz ");
+                    kontrol = true;
+                } else {
+                    kontrol = false;
+                }
+            }
+            System.out.println("===========================");
+            if (this.mineMap[satir][sutun].equals("*")) {
+                System.out.println("Kaybettin ! ");
+                printMineMap(); // Mayın haritasını göster
+                this.durum = false;
+                break;
+            }
+            for (int i = -1; i < 2; i++) {
+                for (int j = -1; j < 2; j++) {
+                    if ((satir + i >= 0 && (satir + i) <= (this.adetSatir - 1)) && (sutun + j >= 0 && (sutun + j) <= (this.adetSutun - 1))) {
+                        if (this.mineMap[satir + i][sutun + j].equals("*")) {
+                            sayac++;
+                            this.board[satir][sutun] = String.valueOf(sayac);
+                        }
+                        if (sayac == 0) {
+                            this.board[satir][sutun] = "0";
+                        }
+                    }
+                }
+            }
+            if (this.durumSayac == this.adetSatir * this.adetSutun - (this.adetSutun * this.adetSatir / 4)) {
+                this.win = true;
+            }
+            if (this.win == true) {
+                this.durum = false;
+                System.out.println("KAZANDINIZ !!!");
+            } else durum = true;
+            printBoard(); // Oyun tahtasını güncel durumuyla ekrana bas
         }
     }
 
-    // Belirtilen konumun etrafındaki mayınları kontrol eden method
-    public void check(int r, int c) {
-        if(map[r][c] == 0) {
-            if((c < colNumber -1) && (map[r][c+1] == -1)) {
-                board[r][c]++;
+    // Mayın haritasını ekrana basan metot
+    public void printMineMap() {
+        for (int i = 0; i < this.adetSatir; i++) {
+            for (int j = 0; j < this.adetSutun; j++) {
+                System.out.print(" " + this.mineMap[i][j]);
             }
-            if((r < rowNumber -3) && (map[r+1][c] == -1)) {
-                board[r][c]++;
-            }
-            if((r > 0) && (map[r-1][c] == -1)) {
-                board[r][c]++;
-            }
-            if((c > 0) && (map[r][c-1] == -1)) {
-                board[r][c]++;
-            }
-            if(board[r][c] == 0) {
-                board[r][c] = -2;
-            }
+            System.out.println("");
         }
-
+        System.out.println("===========================");
+        System.out.println("Mayın Tarlası Oyuna Hoşgeldiniz !");
     }
 
-    // Oyun haritasını hazırlayan method
-    public void prepareGame() {
-        int randRow,randCol,count=0;
-        for(int i=0;i<=(size/4);i++){
-            randRow = rand.nextInt(rowNumber);
-            randCol = rand.nextInt(colNumber);
-            if(map[randRow][randCol]!= -1) {
-                map[randRow][randCol]= -1;
-                count++;
+    // Oyun tahtasını ekrana basan metot
+    public void printBoard() {
+        for (int i = 0; i < this.adetSatir; i++) {
+            for (int j = 0; j < this.adetSutun; j++) {
+                System.out.print(" " + this.board[i][j]);
             }
+            System.out.println("");
         }
     }
 
-    // İki boyutlu bir diziyi ekrana basan method
-    public void print(int[][] arr) {
-        for(int i=0; i<arr.length; i++) {
-            for(int j=0; j<arr.length; j++) {
-                if(arr[i][j]>=0)
-                    System.out.print(" ");
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
-        }
+    // Satır ve sütun sayılarını güncelleyen metot
+    public void MineCheck(int satirNo, int sutunNo) {
+        this.adetSatir = satirNo;
+        this.adetSutun = sutunNo;
     }
-
-
 }
